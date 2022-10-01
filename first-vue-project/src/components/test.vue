e6e7b213dc3d8b6b6c6707fcad3d41b8df9e5976
Jaffar Abbas <54433038+jaffarabbas@users.noreply.github.com>
2022-10-01 23:30:26 +0500

    Inserted src/components/test.vue at 01/10/2022 23:30:26

---
+++ first-vue-project/src/components/test.vue
@@ -0,0 +1,18 @@
+<script setup>
+    const message = "Javascript"
+</script>
+
+<template>
+  <div>
+    <h1>Test Vue 2 {{message}}</h1>
+  </div>
+</template>
+
+<style scoped>
+  h1{
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+  }
+</style>
